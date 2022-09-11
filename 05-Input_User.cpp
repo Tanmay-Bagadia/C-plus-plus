4ba997d1c3967e4742e6ea6950e9243c52b51835
Tanmay Bagadia <tanmaybagadia13@gmail.com>
2022-09-11 12:18:31 +0530

    cin exercise

--- 05-Input_User.cpp
+++ 05-Input_User.cpp
@@ -4,12 +4,21 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter a value for 1st no. : " << endl;
-    cout << "Enter a value for 2nd no. : " << endl;
-    int userVal1;
-    int userVal2;
-    cin >> userVal1 >> userVal2;
-    cout << userVal1 + userVal2;
 
+    // cout << "Enter a value for 1st no. : " << endl;
+    // cout << "Enter a value for 2nd no. : " << endl;
+    // int userVal1;
+    // int userVal2;
+    // cin >> userVal1 >> userVal2;
+    // cout << userVal1 + userVal2;
+
+    // program to convert temperature from farenheit to celsius
+    double farenheitTemp;
+    double celsiusTemp;
+    cout << "Program to convert temperature from farenheit to celsius" << endl;
+    cout << "Enter temperature in Farenheit: " << endl;
+    cin >> farenheitTemp;
+    celsiusTemp = (farenheitTemp - 32.0) * (5 / 9);
+    cout << "Celsius Temperature: " << celsiusTemp;
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+    cout << "Enter a value for 1st no. : " << endl;
+    cout << "Enter a value for 2nd no. : " << endl;
+    int userVal1;
+    int userVal2;
+    cin >> userVal1 >> userVal2;
+    cout << userVal1 + userVal2;

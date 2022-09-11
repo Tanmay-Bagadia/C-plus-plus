0752e627e7c8a47eb1396945056b14240bb938fd
Tanmay Bagadia <tanmaybagadia13@gmail.com>
2022-09-11 12:05:28 +0530

    cin

---
+++ 05-Input_User.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a value for 1st no. : " << endl;
+    cout << "Enter a value for 2nd no. : " << endl;
+    int userVal1;
+    int userVal2;
+    cin >> userVal1 >> userVal2;
+    cout << userVal1 + userVal2;
+
+    return 0;
+}

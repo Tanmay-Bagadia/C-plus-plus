9f0968227fa9e81c706574ad7afdf20f6b66fefb
Tanmay Bagadia <tanmaybagadia13@gmail.com>
2022-09-11 12:44:41 +0530

    Variables types

---
+++ 07-variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double price = 30.45;
+    float sales = 3.57f;
+    auto ghoatal = 4939L;
+    char letter = '3';
+    bool isValid = true;
+    return 0;
+}

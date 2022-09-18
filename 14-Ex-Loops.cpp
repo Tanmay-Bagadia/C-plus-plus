6dca4438f9ec43bb801e3d15e212bbd571808f3f
Tanmay Bagadia <tanmaybagadia13@gmail.com>
2022-09-18 17:53:21 +0530

    Print first 10 natural no.s

---
+++ 14-Ex-Loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Program to Print First 10 natural No.s
+    for (int i = 1; i < 11; i++)
+    {
+        cout << i << "\n";
+    }
+}

7b1b0430143b5d329b64c90b68f246d017e64c62
Tanmay Bagadia <tanmaybagadia13@gmail.com>
2022-09-18 17:48:59 +0530

    Loops

---
+++ 13-Loops.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // int i = 0;
+
+    // // while loop
+
+    // while (i < 5)
+    // {
+    //     cout << i << "\n";
+    //     i++;
+    //     /* code */
+    // }
+
+    // do while
+    // do
+    // {
+    //     cout << i << "\n";
+    //     i++;
+    // } while (i < 5);
+    // return 0;
+
+    // for loop
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << "\n";
+    }
+}
